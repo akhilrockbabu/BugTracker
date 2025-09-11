9a7f550b2affa344c3b47d31fdf565c786c957b3
akhilrockbabu <akhiltheckanath@gmail.com>
2025-09-11 13:05:48 +0530

    Intial Skeleton with UserController

---
+++ Program.cs
@@ -0,0 +1,45 @@
+
+using BugTracker.Api.Repositories;
+using BugTracker.Api.Repositories.Interfaces;
+using BugTracker.Api.Services;
+using BugTracker.Api.Services.Interfaces;
+
+namespace BugTracker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
